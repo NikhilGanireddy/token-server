6d24be3e7f357550528de5e5a3ad561469b0c26c
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-05-27 17:35:51 +0530

    et

--- Models/SampleHostelUser.js
+++ Models/SampleHostelUser.js
@@ -1,32 +1,60 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: String, hallTicket: {
-        type: String, unique: true
-    }, room: {
-        type: String, default: "b42"
-    }, year: {
-        type: String, default: "Final Year"
-    }, batch: {
-        type: String, default: "2020-24"
-    }, branch: {
-        type: String, default: "CSE"
-    }, mobile: {
-        type: Number, default: 9876543210
-    }, motherName: {
-        type: String, default: "Mother Name"
-    }, motherMobile: {
-        type: Number, default: 123456789
-    }, fatherName: {
-        type: String, default: "Father Name"
-    }, fatherMobile: {
-        type: Number, default: 123456789
-    }, address: {
-        type: String, default: 99
-    }, pic: {
-        type: String,
-        default: "https://e1.pxfuel.com/desktop-wallpaper/45/590/desktop-wallpaper-one-piece-luffy-computer.jpg"
-    }, password: String
+  name: String,
+  hallTicket: {
+    type: String,
+    unique: true,
+  },
+  room: {
+    type: String,
+    default: "b42",
+  },
+  year: {
+    type: String,
+    default: "Final Year",
+  },
+  batch: {
+    type: String,
+    default: "2020-24",
+  },
+  branch: {
+    type: String,
+    default: "CSE",
+  },
+  mobile: {
+    type: Number,
+    default: 9876543210,
+  },
+  motherName: {
+    type: String,
+    default: "Mother Name",
+  },
+  motherMobile: {
+    type: Number,
+    default: 123456789,
+  },
+  fatherName: {
+    type: String,
+    default: "Father Name",
+  },
+  fatherMobile: {
+    type: Number,
+    default: 123456789,
+  },
+  address: {
+    type: String,
+    default: 99,
+  },
+  pic: {
+    type: String,
+    default:
+      "https://e1.pxfuel.com/desktop-wallpaper/45/590/desktop-wallpaper-one-piece-luffy-computer.jpg",
+  },
+  password: String,
+  complaints: {
+    type: Array,
+  },
 });
 
 const SampleHostelUserModel = mongoose.model("SampleHostelUsers", UserSchema);
--- index.js
+++ index.js
@@ -11,128 +11,173 @@ const SampleHostelUser = require("./Models/SampleHostelUser");
 
 /////////////////////////////////////////////////////////////
 
-
-
 /////////////////////////////////////////////////////////////
 
 // Middlewears
 app.use(express.json());
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false,
-}));
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true, origin: ["http://localhost:5173", "https://token-server.netlify.app"],
-}));
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "https://token-server.netlify.app"],
+  })
+);
 
 // CONTSANTS
 
-
 const client = require("twilio")(process.env.SID, process.env.AUTHKEY);
-
 
 /////////////////////////////////////////////////////////////
 
 // BCRYPT
 
 const hashPassword = (password) => {
-    return new Promise((resolve, reject) => {
-        bcrypt.genSalt(12, (err, salt) => {
-            if (err) reject(err);
-            bcrypt.hash(password, salt, (err, hash) => {
-                if (err) reject(err);
-                resolve(hash);
-            });
-        });
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) reject(err);
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) reject(err);
+        resolve(hash);
+      });
     });
+  });
 };
 
 const comparePassword = (password, haashed) => {
-    return bcrypt.compare(password, haashed);
+  return bcrypt.compare(password, haashed);
 };
 
 /////////////////////////////////////////////////////////////
 
 // ROUTES
 
+// REGISTER ROUTE
+
+app.post("/register", async (req, res) => {
+  const { name, hallTicket, room, password, year, batch, branch } = req.body;
+
+  try {
+    const createUser = await SampleHostelUser.create({
+      name: name,
+      hallTicket: hallTicket,
+      room: room,
+      password: password,
+      year: year,
+      batch: batch,
+      branch: branch,
+    });
+
+    res.json(createUser);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 // LOGIN ROUTE
 app.post("/login", async (req, res) => {
-    const {name, hallTicket, room, password} = req.body;
-    try {
-        const userExist = await SampleHostelUser.findOne({hallTicket});
-        if (userExist) {
-            const passwordMatch = password === userExist.password;
-            const NameMatch = name === userExist.name;
-            const roomMatch = room === userExist.room;
+  const { name, hallTicket, room, password } = req.body;
+  try {
+    const userExist = await SampleHostelUser.findOne({ hallTicket });
+    if (userExist) {
+      const passwordMatch = password === userExist.password;
+      const NameMatch = name === userExist.name;
+      const roomMatch = room === userExist.room;
 
-            if (passwordMatch && roomMatch && NameMatch) {
-                jwt.sign({
-                    id: userExist._id,
-                    name: userExist.name,
-                    room: userExist.room,
-                    hallTicket: userExist.hallTicket,
-                    branch: userExist.branch,
-                    year: userExist.year,
-                    mobile: userExist.mobile,
-                    pic: userExist.pic,
-                }, process.env.JWT_SECRET, {expiresIn: "6h"}, (err, token) => {
-                    if (err) throw err;
-                    res.cookie("token", token, {
-                        secure: true, sameSite: "none"
-                    }).json(userExist);
-                });
-            } else res.status(401).json({
-                error: "not found",
-            });
+      if (passwordMatch && roomMatch && NameMatch) {
+        jwt.sign(
+          {
+            id: userExist._id,
+            name: userExist.name,
+            room: userExist.room,
+            hallTicket: userExist.hallTicket,
+            branch: userExist.branch,
+            year: userExist.year,
+            mobile: userExist.mobile,
+            pic: userExist.pic,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "6h" },
+          (err, token) => {
+            if (err) throw err;
+            res
+              .cookie("token", token, {
+                secure: true,
+                sameSite: "none",
+              })
+              .json(userExist);
+          }
+        );
+      } else
+        res.status(401).json({
+          error: "not found",
+        });
 
-            // console.log(userExist)
-            // console.log(passwordMatch)
-        } else {
-            res.status(401).json("User Not Found");
-        }
-    } catch (e) {
-        res.json(e);
+      // console.log(userExist)
+      // console.log(passwordMatch)
+    } else {
+      res.status(401).json("User Not Found");
     }
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 // PROFILE ROUTE
 
 app.get("/profile", (req, res) => {
-    const {token} = req.cookies;
+  const { token } = req.cookies;
 
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, {}, async (err, data) => {
-            if (err) throw err;
-            const {_id, name, room, hallTicket, branch, year, mobile, pic} = await SampleHostelUser.findById(data.id);
-            res.json({
-                _id, name, room, hallTicket, branch, year, mobile, pic,
-            });
-        });
-    }
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, {}, async (err, data) => {
+      if (err) throw err;
+      const { _id, name, room, hallTicket, branch, year, mobile, pic } =
+        await SampleHostelUser.findById(data.id);
+      res.json({
+        _id,
+        name,
+        room,
+        hallTicket,
+        branch,
+        year,
+        mobile,
+        pic,
+      });
+    });
+  }
 });
 
 // DASHBOARD ROUTE
 
 app.get("/user/dashboard", async (req, res) => {
-    try {
-        const {name, _id, hallTicket} = req.body;
+  try {
+    const { name, _id, hallTicket } = req.body;
 
-        const UserData = await SampleHostelUser.findOne(hallTicket);
-        res.json(UserData);
-    } catch (e) {
-        console.log(e);
-    }
+    const UserData = await SampleHostelUser.findOne(hallTicket);
+    res.json(UserData);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // COMPLAINTS POST ROUTE
 
 app.post("/user/complaints/sendComplaint", async (req, res) => {
-
-    const {userName, userHallTicket, userBranch, userYear, userMobile, complaint} = req.body
-    client.messages.create({
-        body: `FROM:
+  const {
+    userName,
+    userHallTicket,
+    userBranch,
+    userYear,
+    userMobile,
+    complaint,
+  } = req.body;
+  client.messages.create({
+    body: `FROM:
             ${userName},
             ${userHallTicket},
             ${userMobile},
@@ -144,12 +189,12 @@ app.post("/user/complaints/sendComplaint", async (req, res) => {
             ${complaint}
             
             <------ Ends ------>
-            `, from: "whatsapp:+14155238886", to: 'whatsapp:+918333020599'
-    }).then(() => console.log("Done")).catch(err => {
-        console.log(err)
-    })
-
-})
+            `,
+    from: "whatsapp:+14155238886",
+    to: "whatsapp:+918333020599",
+  });
+  res.json("DAONE");
+});
 
 /////////////////////////////////////////////////////////////
 
@@ -157,8 +202,10 @@ app.post("/user/complaints/sendComplaint", async (req, res) => {
 const port = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(port);
-    console.log(`A site is running on the port ${port} wihh a succesful database connection`);
+  app.listen(port);
+  console.log(
+    `A site is running on the port ${port} wihh a succesful database connection`
+  );
 });
 
 /////////////////////////////////////////////////////////////
